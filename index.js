32712406ed2c3f9fd0d16b0406ceadb9662e704b
sanujz <156044793+sanujz@users.noreply.github.com>
2024-03-10 15:03:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,16 @@
-const { Client } = require('discord.js-selfbot-v13');
-const keep_alive = require('./keep_alive.js');
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
-const client = new Client();
-
-client.on('ready', async () => {
-
-    const rpc = new Discord.RichPresence()
-        .setApplicationId('1193597975042216067')
-        .setType('STREAMING')
-        .setURL(`https://twitch.tv/discord`)
-        .setDetails(`.gg/chillzone`)
-        .setName(`𝓛𝓪`)
-        .setState(`.gg/chillzone`)
-        .setParty({
-            max: 10000,
-            current: 6000,
-            id: Discord.getUUID(),
-        })
-        .setAssetsLargeImage(`mp:attachments/1020344221460332556/1193761221783072838/Snapinsta.app_379197787_838705527971837_1873893431697852041_n_1024.jpg`)
-        .setAssetsLargeText(`Members`)
-        .setAssetsSmallImage(`mp:attachments/1193761529716289646/1193762156626321499/5486-partner-black.png`)
-        .setAssetsSmallText(`.gg/chillzone`)
-        .addButton(`Chill Zone`, `https://discord.gg/chillzone`)
-        .addButton(`Socialized`, `https://discord.gg/socialized`);
-    client.user.setActivity(rpc.toJSON());
-
-    console.log(`${client.user.tag} is ready!`);
+client.once('ready', () => {
+    console.log('Bot is online!');
+    client.user.setPresence({
+        activity: {
+            name: 'Streaming',
+            type: 'STREAMING',
+            url: 'https://www.twitch.tv/discord'
+        },
+        status: 'dnd'
+    });
 });
 
 client.login(process.env.token);

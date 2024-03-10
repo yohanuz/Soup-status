0138c683eb964a343174b8bd8d6d4b5d4db191b8
sanujz <156044793+sanujz@users.noreply.github.com>
2024-03-10 14:09:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,24 +3,30 @@ const keep_alive = require('./keep_alive.js')
 
 // Replace TOKEN with your bot account's token
 const client = new Discord.Client();
+client.on('ready', async () => {
 
-client.on('messageCreate', message => {
-  const args = message.content.trim().split(' ');
-  const command = args.shift().toLowerCase();
+    const rpc = new Discord.RichPresence()
+        .setApplicationId('1193597975042216067')
+        .setType('STREAMING')
+        .setURL(`https://twitch.tv/discord`)
+        .setDetails(`.gg/chillzone`)
+        .setName(`𝓛𝓪`)
+        .setState(`.gg/chillzone`)
+        .setParty({
+            max: 6000,
+            current: 5000,
+            id: Discord.getUUID(),
+        })
+        .setAssetsLargeImage(`mp:attachments/1020344221460332556/1193761221783072838/Snapinsta.app_379197787_838705527971837_1873893431697852041_n_1024.jpg`)
+        .setAssetsLargeText(`Members`)
+        .setAssetsSmallImage(`mp:attachments/1193761529716289646/1193762156626321499/5486-partner-black.png`)
+        .setAssetsSmallText(`.gg/chillzone`)
+        .addButton(`Chill Zone`, `https://discord.gg/chillzone`)
+        .addButton(`Socialized`, `https://discord.gg/socialized`);
+    client.user.setActivity(rpc.toJSON());
 
-  if (message.author.id === '1124984868435148861','885368385595125791' && command === 'setstreaming') {
-    const streamName = args.join(' ');
-
-    // Setting the streaming status and displaying timestamps
-    client.user.setActivity(streamName, {
-      type: 'STREAMING',
-      url: 'https://twitch.tv/discord',
-      status: 'dnd',
-    });
-
-    message.channel.send(`Now streaming **${streamName}**!`);
-  }
-}); 
+    console.log(`${client.user.tag} is ready!`);
+});
 
 client.login(process.env.token);
 

43e9874a3142785ddc1029ae8b38c6c12abd122c
sanujz <156044793+sanujz@users.noreply.github.com>
2024-03-11 17:21:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Client } = require('discord.js-selfbot-v13');
+const { Client, ActivityType } = require('discord.js-selfbot-v13');
 const Discord = require('discord.js-selfbot-v13');
 const keep_alive = require('./keep_alive.js')
 
@@ -6,20 +6,20 @@ const client = new Discord.Client();
 
 const currentIndex = 0;
 const statusMessages = ['Message 1', 'Message 2', 'Message 3'];
-const currentStatus = statusMessages[currentIndex];
-console.log(currentStatus);
 
 function updateStatusAndSendMessages() {
   const currentStatus = statusMessages[currentIndex];
   const nextStatus = statusMessages[(currentIndex + 1) % statusMessages.length];
 
-  cleint.on('ready', () => {
-    cleint.user.setPresence({
+  client.on('ready', () => {
+    client.user.setPresence({
       activities: [{ name: currentStatus, type: ActivityType.Custom}],
       status: 'dnd',
     });
+    currentIndex = (currentIndex + 1) % statusMessages.length;
   });
 }
+
 setInterval(updateStatusAndSendMessages, 5000); // Rotate status every 5 seconds
 
 client.login(process.env.token);

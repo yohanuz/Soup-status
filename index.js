c4c82595b11499c2e55837eef6b7a0c691c6e14e
sanujz <156044793+sanujz@users.noreply.github.com>
2024-03-11 17:08:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
+const { Client } = require('discord.js-selfbot-v13');
 const Discord = require('discord.js-selfbot-v13');
 const keep_alive = require('./keep_alive.js')
 
 const client = new Discord.Client();
 
+const statusMessages = ["Testing","Selfbot"];
 
-client.once('ready', () => {
-    console.log('Bot is online!');
-    client.user.setPresence({
-        activity: {
-            name: 'Streaming',
-            type: 'STREAMING',
-            url: 'https://www.twitch.tv/discord'
-        },
-        status: 'dnd'
+function updateStatusAndSendMessages() {
+  const currentStatus = statusMessages[currentIndex];
+  const nextStatus = statusMessages[(currentIndex + 1) % statusMessages.length];
+
+  cleint.on('ready', () => {
+    cleint.user.setPresence({
+      activities: [{ name: currentStatus, type: ActivityType.Custom}],
+      status: 'dnd',
     });
-});
+  });
+}
+setInterval(updateStatusAndSendMessages, 5000); // Rotate status every 5 seconds
 
 client.login(process.env.token);
